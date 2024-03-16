374c0cb1d8ec9ed3d1ba9648a15f0c5ffb10ca90
Stijn van Houwelingen <stijn@van-houwelingen.net>
2024-03-16 19:36:22 +0100

    Aligned Backend and frontend names. Simplified Javascript code

--- assets/js/submit.js
+++ assets/js/submit.js
@@ -3,7 +3,7 @@ const URL = "http://localhost:8080/signup" // FIXME: prod URL
 // your form
 var form = document.getElementById("signup-form");
 
-// attach event listener
+// // attach event listener
 form.addEventListener("submit", submitSignup, true);
 
 function submitSignup(e) {
@@ -12,38 +12,10 @@ function submitSignup(e) {
 
     let formData = new FormData(e.target);
 
-    let data = {
-        member: {},
-        emergency_contact: {}
-    };
-    
-    data.legalfirstnames = formData.get("full_name");
-
-    data.member.firstname = formData.get("nickname");
-    data.member.infix = formData.get("infix");
-    data.member.lastname = formData.get("surname");
-    data.member.phone = formData.get("phone");
-
-    let bday = formData.get("birthday");
-
-    data.date_of_birth = new Date(bday).toISOString().replace(/[.]\d+[Z]/g, "Z");
-
-    data.address = formData.get("address");
-    data.postal_code = formData.get("zipcode");
-    data.city = formData.get("city_village");
-    data.email = formData.get("mail");
-    data.course = formData.get("education");
-    data.cohort = formData.get("cohort_year");
-
-    data.emergency_contact.firstname = formData.get("emergency_contact_first_name");
-    data.emergency_contact.infix = formData.get("emergency_contact_infix");
-    data.emergency_contact.lastname = formData.get("emergency_contact_surname");
-    data.emergency_contact.phone = formData.get("emergency_contact_phone_number");
-
-    data.iban = formData.get("iban");
-    data.account_holder = formData.get("card_holder");
-
-    data.altcha = formData.get("altcha");
+    var data = {};
+    formData.forEach(function(value, key){
+        data[key] = value;
+    });
 
     if (data.altcha == null) {
         return
