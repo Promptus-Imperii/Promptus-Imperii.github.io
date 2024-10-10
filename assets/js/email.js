26cdeecf434669aff696a85c1f59d3964b6b4835
Stijn van Houwelingen <stijn@van-houwelingen.net>
2024-10-10 17:15:17 +0200

    hotfix: Fixed Altcha not working on browsers other than Firefox desktop. Reverted async and deferred loading to prevent issues with timing.

--- assets/js/email.js
+++ assets/js/email.js
@@ -6,28 +6,31 @@ const formElement = document.getElementById("email-form");
 
 const errorsElement = document.getElementById("errorsDiv");
 
-document.querySelector('#altcha').addEventListener('statechange', (ev) => {
-    // state can be: unverified, verifying, verified, error
-    console.log('state:', ev.detail.state);
-    if (ev.detail.state === 'verified') {
-      fetch(URL, {
-        method: "POST",
-        // Send altcha payload
-        body: JSON.stringify({
-            altcha: ev.detail.payload
-        }),
-    }).then((response) => {
-        response.text().then((body) => {
-            const emailBlock = document.getElementById("email-block");
-            emailBlock.innerHTML = ""; // Clear form
-            var p = document.createElement("p");
-            // Add email adress to it
-            p.textContent = body;
-            emailBlock.appendChild(p);
-        }).catch(displayGenericError)
-    }).catch(displayGenericError)
-    }
-  });
+// attach event listener after DOM has loaded to prevent issues with browsers other than Firefox.
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('altcha').addEventListener('statechange', (ev) => {
+        // state can be: unverified, verifying, verified, error
+        console.log('state:', ev.detail.state);
+        if (ev.detail.state === 'verified') {
+            fetch(URL, {
+                method: "POST",
+                // Send altcha payload
+                body: JSON.stringify({
+                    altcha: ev.detail.payload
+                }),
+            }).then((response) => {
+                response.text().then((body) => {
+                    const emailBlock = document.getElementById("email-block");
+                    emailBlock.innerHTML = ""; // Clear form
+                    var p = document.createElement("p");
+                    // Add email adress to it
+                    p.textContent = body;
+                    emailBlock.appendChild(p);
+                }).catch(displayGenericError)
+            }).catch(displayGenericError)
+        }
+    });
+})
 
 function displayGenericError(error) {
     console.log(error)
